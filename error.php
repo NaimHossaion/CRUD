f8288d1015a93004270e9d2d92562a8f7d66c21e
KhandakerNaim <naimcm@gmail.com>
2019-01-23 23:47:20 +0600

    23-Jan-2018, Error file

--- error.php
+++ error.php
@@ -1,30 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<title>Error</title>
     <meta charset="UTF-8">
-    <title>Error</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery-3.3.1.js" type="text/javascript"></script>
+	<script src="js/popper.js" type="text/javascript"></script>
+    <script src="js/bootstrap.bundle.js" type="text/javascript"></script>
+    <!--<link rel="stylesheet" href="css/bootstrap.css">-->
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css"> <div class="container">   -->
+   <!-- <style type="text/css">
         .wrapper{
             width: 750px;
             margin: 0 auto;
         }
-    </style>
+    </style>-->
 </head>
 <body>
-    <div class="wrapper">
-        <div class="container-fluid">
+    <div class="container"> 
+		<h2>Invalid Request</h2>
+  			<p>Add the alert-link class to any links inside the alert box to create "matching colored links".</p>
+		  <div class="alert alert-danger">
+			<strong>Sorry!</strong> you've made an invalid request. Please <a href="index.php" class="alert-link">go back</a> and try again.
+		  </div>
+		
+       <!-- <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header">
                         <h1>Invalid Request</h1>
                     </div>
-                    <div class="alert alert-danger fade in">
+                    <div class="alert alert-danger" role="alert">
                         <p>Sorry, you've made an invalid request. Please <a href="index.php" class="alert-link">go back</a> and try again.</p>
                     </div>
                 </div>
             </div>        
-        </div>
+        </div>-->
+		
     </div>
 </body>
 </html>
